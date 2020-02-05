8b3028da04a324fa703df61789150cf16f55c8a7
Your Name <kitagawashindai@mbp.local>
2020-02-05 12:07:27 +0900

    modified templates to display posts form database.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-  return render(request,'blog/post_list.html',{})
+  posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+  # requestはインターネットのを介してユーザから受け取った全ての情報
+  # 'posts'は名前,postsはクエリセット(上記の変数)
+  return render(request,'blog/post_list.html',{'posts':posts})
 
